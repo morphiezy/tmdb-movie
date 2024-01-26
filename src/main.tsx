32288e1b1bc25a52fe887e0f7192564e8dc1f559
M Yardan Aqiella <myardan.aqiella22@gmail.com>
2024-01-26 19:11:33 +0700

    add react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,7 @@ import "@/styles/index.css";
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
